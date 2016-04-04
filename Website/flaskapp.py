b4f68cd1bf43b2af96bfd3b2746cdb8ed82ec085
dbrill <dabr9981@colorado.edu>
2016-04-04 16:11:47 -0600

    brought website back up with flask integration

--- Website/flaskapp.py
+++ Website/flaskapp.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
-@app.route('/index')
 def index():
-	return "Hello, World!"
-
-app.run(debug=True)
+	return render_template('Website.html')
+if __name__ == '__main__':
+	app.run(debug=True)
 
