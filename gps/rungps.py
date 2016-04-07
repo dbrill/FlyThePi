309de47a13203ad1da3ba9f3b38077d9fc529a06
CaptainAtlas <daniel.bye@colorado.edu>
2016-04-06 18:53:33 -0600

    Indentation

--- gps/rungps.py
+++ gps/rungps.py
@@ -19,66 +19,59 @@ class GpsPoll(threading.Thread):
         self.current_value = None
         self.running = True
 
-        def run(self):
-            global gpsd
-            while gpspoll.running:
-                gpsd.next() #Gets the next set of data and clears the buffer
+    def run(self):
+        global gpsd
+        while gpspoll.running:
+          gpsd.next() #Gets the next set of data and clears the buffer
 
-        if __name__ == '__main__':
-            gpspoll = GpsPoll() # Thread creation
-            try:
-                gpspoll.start()
-                while True:
-                    # Wait a few seconds to get accurate data
-
-                    os.system('clear')
-
-                    print
-                    print ' GPS reading'
-                    print '----------------------------------------'
-                    print 'Latitude:    ' , gpsd.fix.latitude
-                    print 'Longitude:   ' , gpsd.fix.longitude
-                    print 'Time UTC:    ' , gpsd.utc,' + ', gpsd.fix.time
-                    print 'Altitude (m)' , gpsd.fix.altitude
-
-                    print 'Speed (m/s) ' , gpsd.fix.speed
-                    print 'Climb       ' , gpsd.fix.climb
-                    print 'Track       ' , gpsd.fix.track
-                    print 'Mode        ' , gpsd.fix.mode
-                    print
-                    print 'Satellites:        ' , gpsd.satellites
-
-                    time.sleep(5) #Refresh rate
-
-                except (KeyboardInterrupt, SystemExit): # Control-C functionality
-                print "\nKilling Thread..."
-                gpspoll.running = False
-                gpspoll.join() # Wait for the thread to finish.
-                print "Exiting..."
-
-        def getHeight(self):
-            return gpsd.fix.altitude
+    def getHeight(self):
+        return gpsd.fix.altitude
         
-        def getLatitude(self):
-            return gpsd.fix.latitude
+    def getLatitude(self):
+        return gpsd.fix.latitude
 
-        def getLongitude(self):
-            return gpsd.fix.longitude
+    def getLongitude(self):
+        return gpsd.fix.longitude
 
-        def getPosition(self):
-            return (gpsd.fix.latitude, gpsd.fix.longitude)
+    def getPosition(self):
+        return (gpsd.fix.latitude, gpsd.fix.longitude)
 
-        def getSpeed(self):
-            return gpsd.fix.speed
+    def getSpeed(self):
+        return gpsd.fix.speed
 
-        def getClimb(self):
-            return gpsd.fix.climb
+    def getClimb(self):
+        return gpsd.fix.climb
 
-        def getTrack(self):
-            return gpsd.fix.track
+    def getTrack(self):
+        return gpsd.fix.track
 
+if __name__ == '__main__':
+    gpspoll = GpsPoll() # Thread creation
+    try:
+        gpspoll.start()
+        while True:
+            # Wait a few seconds to get accurate data
 
+            os.system('clear')
 
+            print
+            print ' GPS reading'
+            print '----------------------------------------'
+            print 'Latitude:    ' , gpsd.fix.latitude
+            print 'Longitude:   ' , gpsd.fix.longitude
+            print 'Time UTC:    ' , gpsd.utc,' + ', gpsd.fix.time
+            print 'Altitude (m)' , gpsd.fix.altitude
+            print 'Speed (m/s) ' , gpsd.fix.speed
+            print 'Climb       ' , gpsd.fix.climb
+            print 'Track       ' , gpsd.fix.track
+            print 'Mode        ' , gpsd.fix.mode
+            print
+            print 'Satellites:        ' , gpsd.satellites
 
+            time.sleep(5) #Refresh rate
 
-        
+    except (KeyboardInterrupt, SystemExit): # Control-C functionality
+        print "\nKilling Thread..."
+        gpspoll.running = False
+        gpspoll.join() # Wait for the thread to finish.
+        print "Exiting..."
--- gps/rungps_test.py
+++ gps/rungps_test.py
@@ -3,23 +3,23 @@ import rungps
 
 class RungpsTestCase(unittest.TestCase):
 
-	@classmethod
-	def setUpClass(cls):
-        pass
+    '''    @classmethod
+        def setUpClass(cls):
+            pass
 
-    @classmethod
-    def tearDownClass(cls):
-        pass
+        @classmethod
+        def tearDownClass(cls):
+            pass
 
-    def setUp(self):
-        pass
+        def setUp(self):
+            pass
 
-    def tearDown(self):
-        pass
-
-	def test_init(self):
-		text = "testing for null values"
-		gpspoll = rungps.GpsPoll()
+        def tearDown(self):
+            pass
+    '''
+    def test_init(self):
+    	text = "testing for null values"
+    	gpspoll = rungps.GpsPoll()
         self.assertEqual(gpspoll.running, True, "GPS isn't running!")
 
     def test_HeightValue(self):
