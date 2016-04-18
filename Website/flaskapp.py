c7b2ad6909ccd172e280a053cfb854d7ae649150
dbrill <dabr9981@colorado.edu>
2016-04-18 11:29:54 -0600

    Reintegrating html and css files into flask app. Worked with Bryan Hawthorne

--- Website/flaskapp.py
+++ Website/flaskapp.py
@@ -4,6 +4,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('Website.html')
+
+
+@app.route('/where')
+def where():
+	return render_template('where.html')
+
+@app.route('/home')
+def home():
+	return render_template('Website.html')
+@app.route('/pictures')
+def pics():
+	return render_template('Pictures.html')
+	
+
 if __name__ == '__main__':
 	app.run(debug=True)
-
