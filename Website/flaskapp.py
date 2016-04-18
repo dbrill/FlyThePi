a2062bf73575e37e2370f07f8aee80b56e72a2e6
lucywilkinson <luwi8498@colorado.edu>
2016-04-18 12:59:12 -0600

    Fixed broken "about" link

--- Website/flaskapp.py
+++ Website/flaskapp.py
@@ -17,6 +17,9 @@ def home():
 def pics():
 	return render_template('Pictures.html')
 	
+@app.route('/about')
+def about():
+	return render_template('about.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
